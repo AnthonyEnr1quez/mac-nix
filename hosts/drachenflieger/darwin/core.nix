233145bb712226db884451a4dcbb70279ab0d171
AnthonyEnr1quez <32233059+AnthonyEnr1quez@users.noreply.github.com>
2022-09-19 21:31:24 -0500

    fix some formatting

--- hosts/drachenflieger/darwin/core.nix
+++ hosts/drachenflieger/darwin/core.nix
@@ -1,7 +1,4 @@
-# hosts/YourHostName/default.nix ### this is darwin config
-{ pkgs, ... }:
-{
-
+{ pkgs, ... }: {
   # Make sure the nix daemon always runs
   services.nix-daemon.enable = true;
   # Installs a version of nix, that dosen't need "experimental-features = nix-command flakes" in /etc/nix/nix.conf
@@ -11,7 +8,6 @@
   # you'll need to enable this so nix-darwin creates a zshrc sourcing needed environment changes
   programs.zsh.enable = true;
   # bash is enabled by default
-
 
   users.users.ant = {
     name = "ant";
@@ -24,5 +20,4 @@
       (nerdfonts.override { fonts = [ "Hack" ]; })
     ];
   };
-
 }
