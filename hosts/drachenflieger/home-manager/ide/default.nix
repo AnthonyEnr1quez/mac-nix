b1de963e93bd2ec01c469a52b30b44bebbb57d75
AnthonyEnr1quez <32233059+AnthonyEnr1quez@users.noreply.github.com>
2022-09-21 11:30:48 -0500

    install vscode extension that is not in nixpkgs

--- hosts/drachenflieger/home-manager/ide/default.nix
+++ hosts/drachenflieger/home-manager/ide/default.nix
@@ -1,10 +1,17 @@
 { config, pkgs, ... }: {
+  # TODO default dummy sha: AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
     extensions = with pkgs.vscode-extensions; [
       bbenoist.nix
-      # alphabotsec.vscode-eclipse-keybindings   installed manually
-    ];
+     ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "vscode-eclipse-keybindings";
+          publisher = "alphabotsec";
+          version = "0.16.1";
+          sha256 = "VK4OS7fvpJsHracfHdC7blvh6qV0IJse4vdRud/yT/o=";
+        }
+      ];
   };
 }
