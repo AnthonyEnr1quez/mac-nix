203b898b7e2a89b904044f2de1c49d48a4ba4406
AnthonyEnr1quez <32233059+AnthonyEnr1quez@users.noreply.github.com>
2022-11-03 08:12:55 -0500

    git SSH signing

--- modules/home-manager/git/default.nix
+++ modules/home-manager/git/default.nix
@@ -8,6 +8,14 @@
       core = {
         editor = "vim";
       };
+      commit.gpgSign = true;
+      gpg = {
+        format = "ssh";
+        ssh.defaultKeyCommand = "ssh-add -L";
+        ssh.allowedSignersFile = toString (pkgs.writeText "allowed_signers" ''
+        '');
+      };
+      user.signingkey = "~/.ssh/id_ed25519_github.pub";
     };
 
     ignores = [
