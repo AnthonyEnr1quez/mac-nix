2f8a938c89a1f5a5ca04af4db1d7745bded450a2
AnthonyEnr1quez <32233059+AnthonyEnr1quez@users.noreply.github.com>
2023-06-02 17:09:57 -0500

    clean up

--- profiles/work/default.nix
+++ profiles/work/default.nix
@@ -1,10 +1,3 @@
 { config, lib, pkgs, ... }: {
   user.name = "anthony.enriquez";
-
-  hm = {
-    programs.git = {
-      userEmail = "32233059+AnthonyEnr1quez@users.noreply.github.com";
-      userName = "AnthonyEnr1quez";
-    };
-  };
 }
