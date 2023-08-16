97dfebcb02c6b985e92b4a24691ab355e9245bbc
AnthonyEnr1quez <32233059+AnthonyEnr1quez@users.noreply.github.com>
2023-08-16 16:26:40 -0500

    remove direnv flake overlay, auto create .envrc, env var handling in flake

--- hosts/darwin/anthonyenriquez-MacBook-Pro/default.nix
+++ hosts/darwin/anthonyenriquez-MacBook-Pro/default.nix
@@ -12,6 +12,20 @@
       sessionPath = [
         "$GOPATH/bin"
       ];
+
+      # direnvs
+      # todo get relative path injected somehow
+      file = {
+        "Projects/moov/mf/.envrc".text = ''
+          use flake ~/Projects/nix/dotfiles/hosts/darwin/anthonyenriquez-MacBook-Pro/direnvs/mf
+
+          # fix ld linked errors
+          # https://stackoverflow.com/questions/71112682/ld-warning-dylib-was-built-for-newer-macos-version-11-3-than-being-linked-1
+          # ld: warning: object file (/Users/anthony.enriquez/go/pkg/mod/github.com/confluentinc/confluent-kafka-go/v2@v2.2.0/kafka/librdkafka_vendor/librdkafka_darwin_arm64.a(libcommon-lib-der_ec_key.o)) was built for newer macOS version (12.0) than being linked (11.0)
+          # for some reason, cant override this directly in the flake
+          export MACOSX_DEPLOYMENT_TARGET=12.0
+        '';
+      };
     };
 
     programs = {
--- hosts/darwin/anthonyenriquez-MacBook-Pro/direnvs/mf/flake.nix
+++ hosts/darwin/anthonyenriquez-MacBook-Pro/direnvs/mf/flake.nix
@@ -8,25 +8,7 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-          # todo this can be removed after updating flake.lock
-          # https://search.nixos.org/packages?channel=unstable&show=rdkafka&from=0&size=50&sort=relevance&type=packages&query=rdkafka
-          overlays = [
-            (final: super: {
-              rdkafka = super.rdkafka.overrideAttrs (_: rec {
-                version = "v2.2.0";
-
-                src = super.fetchFromGitHub {
-                  owner = "confluentinc";
-                  repo = "librdkafka";
-                  rev = version;
-                  hash = "sha256-v/FjnDg22ZNQHmrUsPvjaCs4UQ/RPAxQdg9i8k6ba/4=";
-                };
-              });
-            })
-          ];
-        };
+        pkgs = import nixpkgs { inherit system; };
       in
       {
         devShells.default = pkgs.mkShellNoCC {
@@ -42,6 +24,10 @@
             cyrus_sasl
             zstd
           ];
+
+          # env vars
+          BUMPER_PD_PATH = "/Users/anthony.enriquez/Projects/moov/mf/platform-dev";
+          BUMPER_INFRA_PATH = "/Users/anthony.enriquez/Projects/moov/mf/infra";
         };
 
         formatter = pkgs.nixpkgs-fmt;
