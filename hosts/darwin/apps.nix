68e4019eac7c2f1c520198451896e93ab7629d71
AnthonyEnr1quez <32233059+AnthonyEnr1quez@users.noreply.github.com>
2023-09-13 18:06:12 -0500

    this works way better

--- hosts/darwin/apps.nix
+++ hosts/darwin/apps.nix
@@ -1,8 +1,5 @@
-# Source: https://github.com/Atemu/home-manager/blob/e6d905336181ed8f98d48a1f6c9965b77f18e304/modules/targets/darwin.nix
+# Source: https://github.com/nix-community/home-manager/issues/1341#issuecomment-1714800288
 # Ref: https://github.com/LnL7/nix-darwin/issues/214
-# Ref: https://github.com/nix-community/home-manager/issues/1341
-# Ref: https://github.com/nix-community/home-manager/issues/1341#issuecomment-1301534516
-# Ref (using aliases instead): https://github.com/midchildan/dotfiles/blob/472258f8f07b81829c87baf69d7fcf4294cf1aab/home/modules/linkapps.nix
 { config, lib, pkgs, ... }:
 let
   appEnv = pkgs.buildEnv {
@@ -13,17 +10,20 @@ let
 in
 {
   home.activation.addApplications = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    # Install MacOS applications to the user environment.
-    HM_APPS="$HOME/Applications/Home Manager Apps"
+    toDir="$HOME/Applications/Home Manager Apps"
+    fromDir="${appEnv}/Applications/"
 
-    # Reset current state
-    [ -e "$HM_APPS" ] && $DRY_RUN_CMD rm -r "$HM_APPS"
-    $DRY_RUN_CMD mkdir -p "$HM_APPS"
+    [ -e "$toDir" ] && $DRY_RUN_CMD rm -r "$toDir"
+    $DRY_RUN_CMD mkdir "$toDir"
 
-    # .app dirs need to be actual directories for Finder to detect them as Apps.
-    # In the env of Apps we build, the .apps are symlinks. We pass all of them as
-    # arguments to cp and make it dereference those using -H
-    $DRY_RUN_CMD cp --archive -H --dereference ${appEnv}/Applications/* "$HM_APPS"
-    $DRY_RUN_CMD chmod +w -R "$HM_APPS"
+    (
+      cd "$fromDir"
+      for app in *.app; do
+        $DRY_RUN_CMD /usr/bin/osacompile -o "$toDir/$app" -e 'do shell script "open '$fromDir/$app'"'
+        icon="$(/usr/bin/plutil -extract CFBundleIconFile raw "$fromDir/$app/Contents/Info.plist")"
+        $DRY_RUN_CMD mkdir -p "$toDir/$app/Contents/Resources"
+        $DRY_RUN_CMD cp -f "$fromDir/$app/Contents/Resources/$icon" "$toDir/$app/Contents/Resources/applet.icns"
+      done
+    )
   '';
 }
