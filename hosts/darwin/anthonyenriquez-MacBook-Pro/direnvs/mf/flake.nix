4d4ad8401ef63fe2eece0f2f816ff7c399121aa3
AnthonyEnr1quez <32233059+AnthonyEnr1quez@users.noreply.github.com>
2023-09-28 10:21:32 -0500

    bumper in dev env, func to build a shell script, example without func

--- hosts/darwin/anthonyenriquez-MacBook-Pro/direnvs/mf/flake.nix
+++ hosts/darwin/anthonyenriquez-MacBook-Pro/direnvs/mf/flake.nix
@@ -9,6 +9,47 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+
+        bumper = pkgs.buildGoModule rec {
+          pname = "bumper";
+          version = "0.3.0";
+          src = builtins.fetchGit {
+            url = "git@github.com:moovfinancial/bumper.git";
+            ref = "v${version}";
+            rev = "da5e33b2fd07ba3927004d4106f64d4dce5a2c2a";
+          };
+          doCheck = false;
+          vendorSha256 = "sha256-ix72uivcxw2TzYZnifjbjT0sogdZk5PNEEVpfgitJkY=";
+        };
+
+        # Function to create a basic shell script package
+        # https://www.ertt.ca/nix/shell-scripts/#org6f67de6
+        # https://github.com/ponkila/HomestakerOS/blob/56523feb33a4e797a1a12e9e11321b6d4b6ce635/flake.nix#L39
+        mkScriptPackage = { name, deps }:
+          let
+            scriptPath = ./scripts/${name}.sh;
+            script = (pkgs.writeScriptBin name (builtins.readFile scriptPath)).overrideAttrs (old: {
+              buildCommand = "${old.buildCommand}\n patchShebangs $out";
+            });
+          in
+          pkgs.symlinkJoin {
+            inherit name;
+            paths = [ script ] ++ deps;
+            buildInputs = [ pkgs.makeWrapper ];
+            postBuild = "wrapProgram $out/bin/${name} --prefix PATH : $out/bin";
+          };
+
+        scriptDefs = {
+          simple-script = mkScriptPackage {
+            name = "simple-script";
+            deps = [
+              pkgs.cowsay
+              pkgs.ddate
+            ];
+          };
+        };
+        scripts = with builtins; (map (key: getAttr key scriptDefs) (attrNames scriptDefs));
+
       in
       {
         devShells.default = pkgs.mkShellNoCC {
@@ -23,12 +64,15 @@
 
             cyrus_sasl
             zstd
+
+            bumper
           ] ++ [
             (writeScriptBin "testing123" ''
               tmp="bob"
               echo hello $tmp!
+              which bump
             '')
-          ];
+          ] ++ scripts;
 
           # env vars
           BUMPER_PD_PATH = "/Users/anthony.enriquez/Projects/moov/mf/platform-dev";
