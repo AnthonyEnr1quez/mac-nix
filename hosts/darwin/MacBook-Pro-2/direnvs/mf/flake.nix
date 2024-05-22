5fba3a7ba4a30525887d4fbc17aa4ddf89613e8f
AnthonyEnr1quez <32233059+AnthonyEnr1quez@users.noreply.github.com>
2024-05-22 09:12:04 -0500

    update librdkafka to 2.4.0

--- hosts/darwin/MacBook-Pro-2/direnvs/mf/flake.nix
+++ hosts/darwin/MacBook-Pro-2/direnvs/mf/flake.nix
@@ -23,12 +23,12 @@
         };
 
         librdkafka = pkgs.rdkafka.overrideAttrs (_: rec {
-          version = "2.3.0";
+          version = "2.4.0";
           src = pkgs.fetchFromGitHub {
             owner = "confluentinc";
             repo = "librdkafka";
             rev = "v${version}";
-            sha256 = "sha256-F67aKmyMmqBVG5sF8ZwqemmfvVi/0bDjaiugKKSipuA=";
+            sha256 = "sha256-RLjshOnMmCdlv73M8siIvBZ5kVG3v2XttkLVPU+BHY4=";
           };
         });
 
