e1f6db2a40b9fddd15598dfb3fb5feabc2d698a8
AnthonyEnr1quez <32233059+AnthonyEnr1quez@users.noreply.github.com>
2024-06-18 18:11:25 -0500

    add todo

--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -6,6 +6,7 @@
   hm = {
     imports = [ ../../modules/home-manager/kitty ];
 
+    # todo wtf is this, can I delete it??
     disabledModules = [ "targets/darwin/linkapps.nix" ];
 
     programs.vscode = {
