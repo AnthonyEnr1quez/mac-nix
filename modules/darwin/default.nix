2fb7f1c07e698608c3d39b1b2b76a7475755f0c6
Anthony Enriquez <32233059+AnthonyEnr1quez@users.noreply.github.com>
2024-06-18 13:53:49 -0500

    Update base flake.lock (#104)
    * flake.lock: Update
    
    Flake lock file updates:
    
    • Updated input 'darwin':
        'github:lnl7/nix-darwin/315aa649ba307704db0b16c92f097a08a65ec955?narHash=sha256-u3HBinyIyUvL1%2BN816bODpJmSQdgn0Mbb8BprFw7kqo%3D' (2024-06-09)
      → 'github:lnl7/nix-darwin/58b905ea87674592aa84c37873e6c07bc3807aba?narHash=sha256-iMVwdob8F6P6Ib%2BpnhMZqyvYI10ZxmvA885jjnEaO54%3D' (2024-06-15)
    • Updated input 'home-manager':
        'github:nix-community/home-manager/3d65009effd77cb0d6e7520b68b039836a7606cf?narHash=sha256-Sz8Wh9cAiD5FhL8UWvZxBfnvxETSCVZlqWSYWaCPyu0%3D' (2024-06-09)
      → 'github:nix-community/home-manager/0a7ffb28e5df5844d0e8039c9833d7075cdee792?narHash=sha256-sKrD/utGvmtQALvuDj4j0CT3AJXP1idOAq2p%2B27TpeE%3D' (2024-06-16)
    • Updated input 'nixos-wsl':
        'github:nix-community/NixOS-WSL/e3f215e518d52f6f2e68cf713cefe773284e1aa6?narHash=sha256-STKlWaiiFKDybexvQCg5U1%2BDSLRaxT93NwVaiBSEvTI%3D' (2024-06-09)
      → 'github:nix-community/NixOS-WSL/e0a970cbb8c3af05c80ef48a336ad91efd9b2bf6?narHash=sha256-VBeDG3we0bkbFWMyZy%2BwjUkmeDN58pGFzw1dQCTeDV8%3D' (2024-06-15)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/3bcedce9f4de37570242faf16e1e143583407eab?narHash=sha256-WMU6ZRZrBgEUDIF0siu2aIyVAXcxfElSwzZtS/mSpN4%3D' (2024-06-09)
      → 'github:NixOS/nixpkgs/3f84a279f1a6290ce154c5531378acc827836fbb?narHash=sha256-u1fA0DYQYdeG%2B5kDm1bOoGcHtX0rtC7qs2YA2N1X%2B%2BI%3D' (2024-06-13)
    • Updated input 'stable':
        'github:nixos/nixpkgs/9b5328b7f761a7bbdc0e332ac4cf076a3eedb89b?narHash=sha256-1%2Bua0ggXlYYPLTmMl3YeYYsBXDSCqT%2BGw3u6l4gvMhA%3D' (2024-06-06)
      → 'github:nixos/nixpkgs/cc54fb41d13736e92229c21627ea4f22199fee6b?narHash=sha256-US1tAChvPxT52RV8GksWZS415tTS7PV42KTc2PNDBmc%3D' (2024-06-12)
    
    * fix fonts
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -21,10 +21,7 @@
     ];
   };
 
-  fonts = {
-    fontDir.enable = true;
-    fonts = with pkgs; [
-      (nerdfonts.override { fonts = [ "Hack" ]; })
-    ];
-  };
+  fonts.packages = with pkgs; [
+    (nerdfonts.override { fonts = [ "Hack" ]; })
+  ];
 }
