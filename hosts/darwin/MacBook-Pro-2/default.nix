df6490404d4a27bdb118dae21216d4769dbd5abf
AnthonyEnr1quez <32233059+AnthonyEnr1quez@users.noreply.github.com>
2024-07-10 15:24:13 -0500

    MACOSX_DEPLOYMENT_TARGET 13.0 -> 14.0

--- hosts/darwin/MacBook-Pro-2/default.nix
+++ hosts/darwin/MacBook-Pro-2/default.nix
@@ -32,7 +32,7 @@
           # https://stackoverflow.com/questions/71112682/ld-warning-dylib-was-built-for-newer-macos-version-11-3-than-being-linked-1
           # ld: warning: object file (/Users/anthony.enriquez/go/pkg/mod/github.com/confluentinc/confluent-kafka-go/v2@v2.2.0/kafka/librdkafka_vendor/librdkafka_darwin_arm64.a(libcommon-lib-der_ec_key.o)) was built for newer macOS version (12.0) than being linked (11.0)
           # for some reason, cant override this directly in the flake
-          export MACOSX_DEPLOYMENT_TARGET=13.0
+          export MACOSX_DEPLOYMENT_TARGET=14.0
         '';
 
         ".docker/config.json".text = builtins.toJSON {
