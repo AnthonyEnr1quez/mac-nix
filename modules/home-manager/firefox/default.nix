bf17e621ce18148f9e8e9f41794d50c4b08f1798
AnthonyEnr1quez <32233059+AnthonyEnr1quez@users.noreply.github.com>
2024-07-25 16:57:03 -0500

    add other extensions

--- modules/home-manager/firefox/default.nix
+++ modules/home-manager/firefox/default.nix
@@ -5,9 +5,18 @@
 
     profiles.default = {
       isDefault = true;
+
       extensions = with config.nur.repos.rycee.firefox-addons; [
         bitwarden
+        privacy-badger
+        refined-github
+        sidebery
+        ublock-origin
       ];
+
+      settings = {
+        "extensions.autoDisableScopes" = 0;
+      };
     };
   };
 }
