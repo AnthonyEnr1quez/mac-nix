b4fc1be5c98f595c37674cb00e434ed8be2214e6
AnthonyEnr1quez <32233059+AnthonyEnr1quez@users.noreply.github.com>
2024-08-08 16:20:48 -0500

    go 1.22.6

--- hosts/darwin/MacBook-Pro-2/default.nix
+++ hosts/darwin/MacBook-Pro-2/default.nix
@@ -55,10 +55,10 @@
       go = {
         enable = true;
         package = pkgs.go_1_22.overrideAttrs (_: rec {
-          version = "1.22.5";
+          version = "1.22.6";
           src = pkgs.fetchurl {
             url = "https://go.dev/dl/go${version}.src.tar.gz";
-            hash = "sha256-rJxyPyJJaa7mJLw0/TTJ4T8qIS11xxyAfeZEu0bhEvY=";
+            hash = "sha256-nkjZnVGYgleZF9gYnBfpjDc84lq667mHcuKScIiZKlE=";
           };
         });
         goPath = "go";
