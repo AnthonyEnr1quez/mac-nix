b3a445f3b1516c90f11b68203cf1ec3aab4073f4
AnthonyEnr1quez <32233059+AnthonyEnr1quez@users.noreply.github.com>
2024-12-23 13:43:33 -0600

    ope, flip em the other way

--- modules/home-manager/kitty/default.nix
+++ modules/home-manager/kitty/default.nix
@@ -34,9 +34,9 @@ in
 
         tab_bar_edge = "top";
         tab_bar_style = "separator";
-        tab_title_template = "{index}: {title}";
-        active_tab_title_template = "{index}: {title}";
-        # active_tab_title_template = "{index}: {title.split('/')[-1]}";
+        # tab_title_template = "{index}: {title}";
+        tab_title_template = "{index}: {title.split('/')[-1]}";
+        active_tab_title_template = "{index}: {title.split('/')[-1]}";
 
         # https://www.ditig.com/256-colors-cheat-sheet
         # active_tab_foreground = "#000";
