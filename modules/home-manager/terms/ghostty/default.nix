435df65ae8a2d93c370c4198aa1a0398eea8480a
AnthonyEnr1quez <32233059+AnthonyEnr1quez@users.noreply.github.com>
2025-01-02 12:46:09 -0600

    other config

--- modules/home-manager/terms/ghostty/default.nix
+++ modules/home-manager/terms/ghostty/default.nix
@@ -16,10 +16,26 @@ in
     programs.ghostty = {
       enable = true;
       enableZshIntegration = true;
+      installBatSyntax = false; #TODO
 
       settings = {
-        theme = "catppuccin-mocha";
+        auto-update = "off";
+        clipboard-trim-trailing-spaces = true;
+        font-family = "Hack Nerd Font";
+        window-title-font-family = "Hack Nerd Font";
         font-size = 18;
+
+        macos-option-as-alt = true;
+        quit-after-last-window-closed = true;
+        scrollback-limit = 10000;
+
+        theme = "catppuccin-mocha";
+
+        # does not work with full screen apps :(
+        keybind = "global:alt+space=toggle_quick_terminal";
+        quick-terminal-position = "right";
+        quick-terminal-animation-duration = 0;
+        quick-terminal-autohide = false;
       };
     };
   };
